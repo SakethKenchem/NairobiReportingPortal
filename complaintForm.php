01f22753ca886d11c7bd67a31bd020cd551f4318
[SakethKenchem] <[s.kenchem@gmail.com]>
2024-01-31 21:55:41 +0300

    added ability to preview images to be uploaded

--- complaintForm.php
+++ complaintForm.php
@@ -231,6 +231,31 @@
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<script>
+    // Function to preview images before upload
+    function previewImages(event) {
+        var imagePreviewContainer = document.getElementById("imagePreviewContainer");
+        imagePreviewContainer.innerHTML = "";
+
+        var files = event.target.files;
+
+        for (var i = 0; i < files.length; i++) {
+            var imageElement = document.createElement("img");
+            imageElement.classList.add("img-fluid");
+            imageElement.file = files[i];
+            imagePreviewContainer.appendChild(imageElement);
+
+            var reader = new FileReader();
+            reader.onload = (function(aImg) {
+                return function(e) {
+                    aImg.src = e.target.result;
+                };
+            })(imageElement);
+            reader.readAsDataURL(files[i]);
+        }
+    }
+</script>
 <script> 
 
     function validateForm(){
