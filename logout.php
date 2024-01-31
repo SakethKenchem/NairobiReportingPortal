80555c0de44700ed7b7e6b5b1f5c7a5b9223a284
[SakethKenchem] <[s.kenchem@gmail.com]>
2024-01-31 17:46:23 +0300

    fixed logout logic

--- logout.php
+++ logout.php
@@ -14,8 +14,7 @@ if (isset($_SESSION['userid'])) {
 
     // Update logout time and set manual_logout based on parameter
     $logout_time = date("Y-m-d H:i:s"); // Current timestamp
-    $manual_logout = isset($_POST['manual_logout']) ? $_POST['manual_logout'] : 1;
-    $update_logout_sql = "UPDATE userloginhistory SET logout_time = '$logout_time', manual_logout = $manual_logout WHERE userid = $userid AND logout_time IS NULL";
+    $update_logout_sql = "UPDATE userloginhistory SET logout_time = '$logout_time'WHERE userid = $userid AND logout_time IS NULL";
     mysqli_query($conn, $update_logout_sql);
 }
 
